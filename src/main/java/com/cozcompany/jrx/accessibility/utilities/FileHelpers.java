5f735a937205ca55de8656b8a29f0894a75e0d85
Coz <Coz@localhost>
2020-06-13 22:43:03 -0400

    Hacked FileHelpers.java so that rigctld is found at /usr/local/bin. Added command line args to Run so that the rig comms are printed to out. Now, I can set breakpoints in code while running in NetBeans. Otherwise I would be debugging and running from the command line.

--- src/main/java/com/cozcompany/jrx/accessibility/utilities/FileHelpers.java
+++ src/main/java/com/cozcompany/jrx/accessibility/utilities/FileHelpers.java
@@ -7,12 +7,36 @@ import java.nio.file.Paths;
 public class FileHelpers {
     public String findAbsolutePath( String curl ){
         String path = System.getenv( "PATH" );
+        System.out.println("System.getenv(PATH) returns :" + path);
         String[] dirs = path.split( ";|:" );
+        
+//        ////////////////////////////////////////////////////BEGIN HACK
+//        // HACK - DO NOT RELEASE THIS CODE.
+//        ///////////////////////////////        
+//        Path aCurl = Paths.get(  "/user/local/bin", curl );
+//        String commandString = aCurl.toAbsolutePath().toString();
+//        File aFile = new File( aCurl.toString() ); 
+//        if(aFile.canExecute() ) return aCurl.toAbsolutePath().toString();        
+//        System.out.println("command : "+ commandString);
+//        ///////////////////////////////////////////////////END HACK
+        
+        File rigctldFile = new File("/usr/local/bin/rigctld");
+        if ( rigctldFile.canExecute()) {
+            System.out.println("can Execute() : "+ rigctldFile.toString());
+            return rigctldFile.toString();
+        }
+
+
+
+
         for( String dir: dirs ){
             Path toCurl = Paths.get( dir, curl );
-            File curlFile = new File( toCurl.toString() );
-            if( curlFile.canExecute() ) return toCurl.toAbsolutePath().toString();
+            File curlFile = new File( toCurl.toString() );           
+            if( curlFile.canExecute() ) {
+                System.out.println("can Execute() : "+ toCurl.toAbsolutePath().toString());          
+                return toCurl.toAbsolutePath().toString();
+            }
         }
-        return null;
+        return null; /// THIS SHOULD BE return null;
     }
 }
